e91fc9c0e1149ca3119ccde1ef81bf5341bb0852
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-10-29 17:04:42 +0700

    block-home-page-without-email

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 export default function HomePage() {
+    const history = useHistory()
     const email = useSelector (state => state.email)
+    if (!email) {
+        history.push("/")
+    }
     return (
         <div>
             <h1>Welcome</h1>
